384fc7441891126eabe04663c15b23a97dbd6980
Rong <rongxie1119@gmail.com>
2013-09-05 22:59:52 -0700

    add comments

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,59 +1,90 @@
 package percolation;
 
+/*----------------------------------------------------------------
+ *  Author:        RONG XIE
+ *  Written:       9/5/2013
+ *  Last updated:  9/5/2013
+ *
+ *  Compilation:   javac Percolation.java
+ *  Execution:     NONE
+ *  
+ *  Purpose:  provide API for Percolation Threshold Simulation
+ *----------------------------------------------------------------*/
+
 public class Percolation {
+    private int N; // the number of each site
+    private WeightedQuickUnionUF u;  // set data structure 
+    private boolean[] openSite;  // array for indicating the opensite
+    private static final int LEFT = 0; // left indicator
+    private static final int RIGHT = 1;  // right indicator
+    private static final int TOP = 2;  // top indicator
+    private static final int DOWN = 3;  // down indicator
+    
+    /**
+     * 
+     * @param N size of the grid in 1d
+     * Constructor
+     */
+    public Percolation(int N) {
+        this.N = N;
+        u = new WeightedQuickUnionUF(N * N);
+        this.openSite = new boolean[N * N];
+        for (int i = 0; i < N * N; i++)
+            this.openSite[i] = false;
+    }
 	
-	private int N;
-	private WeightedQuickUnionUF u;
-	private boolean[] openSite;
-	private static final int LEFT = 0;
-	private static final int RIGHT = 1;
-	private static final int TOP = 2;
-	private static final int DOWN = 3;
+    /**
+     * open grid(i,j) if not opened
+     * @param i row index
+     * @param j column index
+     * indicate the open status of specified grid
+     */
+    public void open(int i, int j)  {
+        validateIndex(i, j);
+        openSite[getGrid(i, j)] = true;
+        for (int direction = LEFT; direction <= DOWN; direction++)
+          check(i, j, direction);
+    }
+    
+    /**
+     * check grid (i,j) open status
+     * @param i row index
+     * @param j column index
+     * @return true if grid (i,j) is open 
+     */
+    public boolean isOpen(int i, int j)  {
+        validateIndex(i, j);
+        return openSite[getGrid(i, j)];
+    }
 	
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N) {
-		this.N = N;
-		u = new WeightedQuickUnionUF(N*N);
-		this.openSite = new boolean[N*N];
-		for (int i = 0; i < N*N; i++)
-			this.openSite[i] = false;
-	}
-	   
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j)  {
-		if (!validateIndexes(i,j)) return;
-		openSite[getGrid(i,j)] = true;
-		for (int direction = LEFT; direction <= DOWN; direction++)
-			check(i,j, direction);
-		
-	}
-	   
-	   // is site (row i, column j) open?
-	public boolean isOpen(int i, int j)  {
-	   if (!validateIndexes(i,j)) return false;
-	   return openSite[getGrid(i,j)];
-	}
-	   
-	   // is site (row i, column j) full?
-	public boolean isFull(int i, int j)   {
-		
-		int grid = getGrid(i,j);
-		for (int k = 0; k < N; k++) {
-			if(openSite[k]) {
-				if (u.connected(k, grid))
-					return true;
-			}
-		}
-		return false;
-	}
-	   
-	   // does the system percolate?
+    /**
+     * check grid (i,j) full status
+     * @param i row index
+     * @param j column index
+     * @return true if grid (i,j) is full
+     */
+    public boolean isFull(int i, int j)   {
+        validateIndex(i, j);
+        int grid = getGrid(i, j);
+        for (int k = 0; k < N; k++) {
+            if(openSite[k]) {
+                if (u.connected(k, grid))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * check if the system percolate
+     * @return true if the system percolate
+     */
 	public boolean percolates() {
 	  for (int i = 1; i <= N; i++) {
-		  int p = getGrid(N,i);
+		  int p = getGrid(N, i);
 		  if (openSite[p]) {
 			  for (int j = 1; j <= N; j++) {
-				  int q = getGrid(1,j);
+				  int q = getGrid(1, j);
 				  if (openSite[q]) {
 					  if (u.connected(p, q)) return true;
 				  }
@@ -63,34 +94,81 @@ public class Percolation {
 	  return false;
 	}
 	
-	// validate indexes
-	private boolean validateIndexes(int i, int j) {
-		if (i <= 0 || j <= 0 || i > N || j > N) return false;
-		return true;
-	}
+	/**
+	 * validate indexes with throwing exception
+	 * @param i row index
+	 * @param j column index
+	 * @return true if validation is positive
+	 */
+    private boolean validateIndex(int i, int j) {
+        if (i <= 0 || j <= 0 || i > N || j > N) throw new IndexOutOfBoundsException("indexes i or j out of bounds");
+        return true;
+    }
 	
-	private int getGrid(int i, int j) {
-		return (i-1) * N + (j-1);
-	}
+    /**
+     * validate indexes without throwing exception
+     * @param i row index
+     * @param j column index
+     * @return true if validation is positive, false otherwise
+     */
+    private boolean validateIndexes(int i, int j) {
+        if (i <= 0 || j <= 0 || i > N || j > N) { 
+            return false;
+        }
+        return true;
+    }
 	
-	private void check(int i, int j, int direction) {
-		int p = 0, q = 0;
-		switch (direction) {
-		case LEFT:
-			p = i; q = j-1; break;
-		case RIGHT:
-			p = i; q = j+1; break;
-		case TOP:
-			p = i-1; q = j; break;
-		case DOWN:
-			p = i+1; q = j; break;
-		}
-		if (!validateIndexes(p,q)) return;
-		if (openSite[getGrid(p,q)])merge(i,j,p,q);
-	}
+    /**
+     * compute grid index in 1d
+     * @param i row index
+     * @param j column index
+     * @return index of grid in 1d
+     */
+    private int getGrid(int i, int j) {
+        return (i - 1) * N + (j - 1);
+    }
 	
-	private void merge(int i, int j, int p, int q) {
-		//System.out.println("merging ("+i+","+j+") and ("+p+","+q+")");
-		u.union(getGrid(i,j), getGrid(p,q));
-	}
+    /**
+     * check if neighbors are open and update connected parts
+     * @param i row index
+     * @param j column index
+     * @param direction direction of neighbors
+     */
+    private void check(int i, int j, int direction) {
+        int p = 0;
+        int q = 0;
+        switch (direction) {
+        case LEFT: 
+            p = i; 
+            q = j - 1; 
+            break;
+        case RIGHT: 
+            p = i; 
+            q = j + 1; 
+            break;
+        case TOP: 
+            p = i - 1; 
+            q = j; 
+            break;
+        case DOWN: 
+            p = i + 1; 
+            q = j; 
+            break;
+        }
+        if (!validateIndexes(p, q)) 
+            return;
+        if (openSite[getGrid(p, q)]) 
+            merge(i, j, p, q);
+    }
+	
+    /**
+     * merge two grid together
+     * @param i row index for grid 1
+     * @param j column index for grid 1
+     * @param p row index for grid 2
+     * @param q column index for grid 2
+     */
+    private void merge(int i, int j, int p, int q) {
+        u.union(getGrid(i, j), getGrid(p, q));
+    }
 }
--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -1,80 +1,109 @@
 package percolation;
 
+/*----------------------------------------------------------------
+ *  Author:        RONG XIE
+ *  Written:       9/5/2013
+ *  Last updated:  9/5/2013
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats 100 50
+ *  
+ *  Purpose: show statics for Percolation Sim in 100 * 100 grid 
+ *           for 50 trials
+ *----------------------------------------------------------------*/
 
 public class PercolationStats {
-	// perform T independent computational experiments on an N-by-N grid
-	private static double[] x;
-	private int N = 0;
-	private int T = 0;
-	private double mean = 0;
-	private double stddev = 0;
-	private double confLow = 0;
-	private double confHigh = 0;
-	
-	   public PercolationStats(int N, int T)    {
-		   if (N >= 1 && T >= 1) {
-			   this.N = N;
-			   this.T = T;
-			   x = new double[T];
-		   }
-	   }
-	   // sample mean of percolation threshold
-	   public double mean()  {
-		   double sum = 0;
-		   for (int i = 0; i < T; i++) {
-			   sum += x[i];
-		   }
-		   mean = sum/T;
-		   return mean;
-	   }
-	   
-	   // sample standard deviation of percolation threshold
-	   public double stddev()  {
-		   if (T == 1) return Double.NaN;
-		   double sum = 0;
-		   for (int i = 0; i < T; i++) {
-			   sum += (x[i] - mean)*(x[i]-mean);
-		   }
-		   stddev = sum / (T-1);
-		   return stddev;
-	   }
-	   
-	   // returns lower bound of the 95% confidence interval
-	   public double confidenceLo()  {
-		   confLow = mean - 1.96*Math.sqrt(stddev)/Math.sqrt(T);
-		   return confLow;
-	   }
-	   
-	   // returns upper bound of the 95% confidence interval
-	   public double confidenceHi()   {
-		   confHigh = mean + 1.96*Math.sqrt(stddev)/Math.sqrt(T);
-		   return confHigh;
-	   }
-	   
-	   // test client, described below
-	   public static void main(String[] args)   {
-		   int N = Integer.parseInt(args[0]);
-		   int T = Integer.parseInt(args[1]);
-		   if (N < 1 || T < 1) return;
-		   PercolationStats stats = new PercolationStats(N,T);
-		   for (int i = 0; i < T; i++) {
-			   int count = 0;
-			   Percolation perc = new Percolation(N);
-			   while(!perc.percolates()) {
-				   int row = StdRandom.uniform(stats.N)+1;
-				   int col = StdRandom.uniform(stats.N)+1;
-				   if (!perc.isOpen(row, col)) {
-					   perc.open(row, col);
-					   count++;
-				   }
-			   }
-			   //System.out.println("count:"+count+",N*N:"+N*N);
-			   x[i] = ((double)count)/(N*N);
-			   //System.out.println(x[i]);
-		   }
-		   
-		   System.out.println("mean                    = "+stats.mean());
-		   System.out.println("stddev                  = "+stats.stddev());
-		   System.out.println("95% confidence interval = "+stats.confidenceLo()+", "+stats.confidenceHi());
-	   }
+    private double[] x;  // record the threshold for T trials
+    private int T = 0;  // # of trials
+    
+    /**
+     * Constructor
+     * @param N size of grid in 1d
+     * @param T number of trials
+     */
+    public PercolationStats(int N, int T) {	
+        if (N >= 1 && T >= 1) {
+            this.T = T;
+            x = new double[T];
+        }
+        for (int i = 0; i < T; i++) {
+            int count = 0;
+            Percolation perc = new Percolation(N);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniform(N) + 1;
+                int col = StdRandom.uniform(N) + 1;
+                if (!perc.isOpen(row, col)) {
+                    perc.open(row, col);
+                    count++;
+                }
+            }
+            x[i] = ((double) count) / (N * N);
+        }
+        System.out.println("mean                    = " + mean());
+        System.out.println("stddev                  = " + stddev());
+        System.out.println("95% confidence interval = " + confidenceLo()
+				+ ", " + confidenceHi());
+    }
+    
+    /**
+     * compute the mean of current simulation
+     * @return mean of current simulation
+     */
+    public double mean() {
+        double sum = 0;
+        for (int i = 0; i < T; i++) {
+            sum += x[i];
+        }
+        return sum / T;
+    }
+    
+    /**
+     * compute the standard deviation of percolation threshold
+     * @return stddev for current simulation
+     */
+    public double stddev() {
+        if (T == 1)
+            return Double.NaN;
+        double mean = mean();
+        double sum = 0;
+        for (int i = 0; i < T; i++) {
+            sum += (x[i] - mean) * (x[i] - mean);
+        }
+        double test = Math.sqrt(sum / (T - 1));
+        System.out.println(test);
+        System.out.println(sum / (T - 1));
+        return sum / (T - 1);
+    }
+    
+    /**
+     * compute the confidence low bound
+     * @return lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {
+        double mean = mean();
+        double stddev = stddev();
+        return mean - 1.96 * Math.sqrt(stddev) / Math.sqrt(T);
+    }
+
+    /**
+     * compute the confidence high bound
+     * @return upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {
+        double mean = mean();
+        double stddev = stddev();
+        double confHigh = mean + 1.96 * Math.sqrt(stddev) / Math.sqrt(T);
+        return confHigh;
+    }
+    
+    /**
+     * test client
+     * @param args
+     */
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        if (N < 1 || T < 1) throw new IllegalArgumentException("N or T are illegal");
+        PercolationStats stats = new PercolationStats(N, T);
+    }
 }
